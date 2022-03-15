58adb0f5fba6b506370bdc3ce8b0ba390148aea0
andras.csecsi <andras.csecsi@bca.hu>
2022-03-15 19:40:58 +0100

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,12 @@ import App from "./App";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn reeact/i);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
